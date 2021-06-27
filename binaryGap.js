5ac2c569181fbbeb90f6a721ae20d8bf47909cbd
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-27 13:57:27 -0700

    attempt 2 100%

--- binaryGap.js
+++ binaryGap.js
@@ -9,14 +9,13 @@ const gaps = (bin) => {
   for (let i = 0; i < bin.length; i++) {
       if (bin[i] == '1') {
           if (open) {
-              open = false;
               gaps.push(currentCount - 0);
               currentCount = 0;
           } else {
               open = true;
           }
       } else if (bin[i] == '0' && open) currentCount++;
-  }
+  }    
   gaps.sort((a, b) => b - a);
   return gaps[0] ? gaps[0] : 0;
 }
@@ -24,6 +23,5 @@ const gaps = (bin) => {
 function solution(N) {
   // write your code in JavaScript (Node.js 8.9.4)
   const bin = binary(N);
-  
   return gaps(bin);
 }
