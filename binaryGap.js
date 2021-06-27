c3abd7f7f0f256ae77de59fd018d645df4fed603
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-27 13:36:21 -0700

    first attempt binary gap solution, missed a couple edge cases

---
+++ binaryGap.js
@@ -0,0 +1,29 @@
+const binary = (N) => {
+  return (N).toString(2)
+}
+
+const gaps = (bin) => {
+  let open = false;
+  let gaps = [];
+  let currentCount = 0;
+  for (let i = 0; i < bin.length; i++) {
+      if (bin[i] == '1') {
+          if (open) {
+              open = false;
+              gaps.push(currentCount - 0);
+              currentCount = 0;
+          } else {
+              open = true;
+          }
+      } else if (bin[i] == '0' && open) currentCount++;
+  }
+  gaps.sort((a, b) => b - a);
+  return gaps[0] ? gaps[0] : 0;
+}
+
+function solution(N) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  const bin = binary(N);
+  
+  return gaps(bin);
+}
