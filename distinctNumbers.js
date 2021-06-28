244be5f3a5c02912c134df0bfd1474fe969972e5
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-28 12:34:23 -0700

    O^N time complexity

--- distinctNumbers.js
+++ distinctNumbers.js
@@ -1,7 +1,15 @@
 function solution(A) {
-  const newArray = [];
-  A.filter(e => {
-      if (!newArray.includes(e)) newArray.push(e);
-  })
-  return newArray.length;
+  const set = new Set();
+  for (let i = 0; i < A.length; i++) {
+      set.add(A[i]);
+  }
+  return set.size;
 }
+
+// function solution(A) {
+//   const newArray = [];
+//   A.filter(e => {
+//       if (!newArray.includes(e)) newArray.push(e);
+//   })
+//   return newArray.length;
+// }
