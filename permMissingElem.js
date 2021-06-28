bf8ff988d47181f0d06397ec56169841d169b963
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-27 21:47:23 -0700

    fixed edge cases

--- permMissingElem.js
+++ permMissingElem.js
@@ -1,8 +1,9 @@
 function solution(A) {
-  if (A.length < 1) return -1;
+  if (A.length < 1) return 1; //empty array edge case
   A.sort((a,b) => a-b);
+  if (A[0] != 1) return 1; //first is missing edge case
   for (let i = 0; i < A.length; i++) {
       if (A[i] + 1 != A[i+1]) return A[i] + 1;
   }
-  return A[length - 1] + 1;
+  return A[length - 1] + 1; //last is missing
 }
